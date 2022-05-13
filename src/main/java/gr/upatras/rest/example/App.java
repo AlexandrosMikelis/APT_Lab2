90b8f6359ebde174b63cb04a417233cbce18fc2e
Alexandros Mikelis <iamalekos@hotmail.com>
2022-05-13 18:01:50 +0300

    Init Commit

---
+++ src/main/java/gr/upatras/rest/example/App.java
@@ -0,0 +1,11 @@
+package gr.upatras.rest.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
+
